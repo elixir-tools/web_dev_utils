b55fec9bdb99b538058a8d1058df22a7ca06fc83
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-10-28 13:57:03 -0400

    chore: description

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule WebDevUtils.MixProject do
   def project do
     [
       app: :web_dev_utils,
+      description: "Library to enable awesome local development for websites and web applications",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
