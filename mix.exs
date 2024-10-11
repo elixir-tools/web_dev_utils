f944653f6386aa3c459dbef2db2f95837d831777
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-10-11 14:10:16 -0400

    fixup! update version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,8 +6,9 @@ defmodule WebDevUtils.MixProject do
   def project do
     [
       app: :web_dev_utils,
-      description: "Library to enable awesome local development for websites and web applications",
-      version: "0.1.1",
+      description:
+        "Library to enable awesome local development for websites and web applications",
+      version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       package: package(),
